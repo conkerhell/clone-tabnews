cf6ce21d67dcf75a92fd07a7bc0dddef6bf7a750
Leonardo Ines <32177823+conkerhell@users.noreply.github.com>
2024-12-17 22:52:07 +0000

    Adicionando emoji

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home() {
    return <><h1>Rackel, If U love me &#129392;, give me a smile!! &#128512;</h1> 
-   <p>We are criminals that never broke no laws</p>
-   <h2>teste</h2></>
+   <h2>We are criminals that never broke no laws 🙉🙈🙊</h2></>
    
    
 }
