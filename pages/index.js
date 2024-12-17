baff3295c014c2f48f9dc237aa1dcce0dfcdd187
Leonardo Ines <32177823+conkerhell@users.noreply.github.com>
2024-12-17 22:47:34 +0000

    Adicionando quebra de linha e um novo paragrafo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home() {
-   return <h1>Rackel, If U love me &#129392;, give me a smile!! &#128512;</h1>;
-   <p>We are criminals that never broke no laws</p>
+   return <><h1>Rackel, If U love me &#129392;, give me a smile!! &#128512;</h1> 
+   <p>We are criminals that never broke no laws</p></>
+   
+   
 }
 
 export default Home;
