6e770c174ee76b75869b24b38cc199b985f096d3
Leonardo Ines <32177823+conkerhell@users.noreply.github.com>
2024-12-17 22:49:38 +0000

    Testando quebra de linha

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
    return <><h1>Rackel, If U love me &#129392;, give me a smile!! &#128512;</h1> 
-   <p>We are criminals that never broke no laws</p></>
+   <p>We are criminals that never broke no laws</p>
+   <h2>teste</h2></>
    
    
 }
