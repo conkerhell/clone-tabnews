f31433f93f6a7946589db6764e93e4f06a9a45ab
Leonardo Ines <32177823+conkerhell@users.noreply.github.com>
2024-12-17 22:43:32 +0000

    Incluindo um novo paragrafo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-   return <h1>Rackel, If U love me &#129392;, give me a smile!! &#128512;</h1>  
+   return <h1>Rackel, If U love me &#129392;, give me a smile!! &#128512;</h1>;
+   <p>We are criminals that never broke no laws</p>
 }
 
 export default Home;
